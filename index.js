93d91686a7ef6f5974613aa07593c8f2b028fa35
Foysal79 <onthor312001@gmail.com>
2023-10-20 14:10:27 +0600

    items send database post methode

--- index.js
+++ index.js
@@ -26,12 +26,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("highStyleDB");
+    const userCollection = database.collection('items');
+
+    app.post('/items', async(req, res) => {
+        const item = req.body;
+        console.log('new item', item);
+        const result = await userCollection.insertOne(item);
+        res.send(result);
+
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
